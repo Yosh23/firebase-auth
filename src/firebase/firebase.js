4f1eed531cbf1db20b56a5cf6e621e32889f9b5b
Josh Riches <joshriches@gmail.com>
2018-08-02 11:05:11 -0600

    don't need a different database for dev & production

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const prodConfig = {
+const config = {
     apiKey: YOUR_API_KEY,
     authDomain: YOUR_AUTH_DOMAIN,
     databaseURL: YOUR_DATABASE_URL,
@@ -9,19 +9,6 @@ const prodConfig = {
     storageBucket: '',
     messagingSenderId: YOUR_MESSAGING_SENDER_ID,
 };
-
-const devConfig = {
-    apiKey: YOUR_API_KEY,
-    authDomain: YOUR_AUTH_DOMAIN,
-    databaseURL: YOUR_DATABASE_URL,
-    projectId: YOUR_PROJECT_ID,
-    storageBucket: '',
-    messagingSenderId: YOUR_MESSAGING_SENDER_ID,
-};
-
-const config = process.env.NODE_ENV === 'production'
-    ? prodConfig
-    : devConfig;
 
 if (!firebase.apps.length) {
     firebase.initializeApp(config);
